9e9b3642f7fb70732e4c5d5b2200db19a310de90
arthur <arthur.rios007@gmail.com>
2025-02-27 20:14:46 -0300

    feat: deliver model as API

---
+++ api_reg_model.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+from fastapi import FastAPI
+import uvicorn
+import joblib
+
+# Create instance of FastAPI
+app = FastAPI()
+
+# Create a class for the request body
+class request_body(BaseModel):
+  study_hours : float
+  
+# Load the model
+grade_model = joblib.load('./reg_model.pkl')
+
+def predict(data : request_body):
+  # Prepare data for prediction
+  input_feature = [[data.study_hours]]
+  
+  # Make prediction
+  y_pred = grade_model.predict(input_feature)[0].astype(int)
+  
+  return {'test_grade': y_pred.tolist()}
